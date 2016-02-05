c855df6acb108bf05b506321d1bba423a33f3e09
cmal <zyzy5730@163.com>
2016-02-05 14:51:21 +0800

    *** empty log message ***

--- init.el
+++ init.el
@@ -11,13 +11,17 @@
 ;; 说明: .bash_profile is loaded for your login shell only. If you want to customize regular shells (such as xterm windows, or Emacs shells), you need to put the customization in .bashrc instead. Many people will source .bashrc from .bash_profile, so that you get all of your customizations in your login shell, but only those in .bashrc in your regular shells.
 (setq-default shell-command-switch "-ic")
 (setq-default current-language-environment "English")
-;(setq system-time-locale "en_US") ??
+(setq system-time-locale "en_US")
 
 ;; melpa
 (require 'package) ;; You might already have this line
 (add-to-list 'package-archives
              '("melpa" . "https://melpa.org/packages/"))
 (package-initialize)
+
+(add-to-list 'custom-theme-load-path "~/.emacs.d/elpa/moe-theme-20151124.1509")
+(add-to-list 'load-path "~/.emacs.d/elpa/moe-theme-20151124.1509")
+
 
 ;;  -----------------------------------
 ;; | Section 0: Debugging              |
@@ -68,7 +72,7 @@
 (global-set-key [f8] 'neotree-toggle)
 
 
-(add-to-list 'default-frame-alist '(width  . 81))
+(add-to-list 'default-frame-alist '(width  . 121))
 (add-to-list 'default-frame-alist '(height  . 40))
 
 ;; txt结尾的文件的名字优先用中文编码显示
@@ -112,16 +116,87 @@
  '(ansi-color-names-vector
    ["#2e3436" "#a40000" "#4e9a06" "#c4a000" "#204a87" "#5c3566" "#729fcf" "#eeeeec"])
  '(cfs--current-profile-name "profile3" t)
- '(cfs--fontsize-steps (quote (3 4 5)) t)
- '(custom-enabled-themes (quote (zenburn)))
+ '(cfs--fontsize-steps (quote (3 4 4)) t)
+ '(compilation-message-face (quote default))
+ '(custom-enabled-themes (quote (moe-light)))
  '(custom-safe-themes
    (quote
-    ("f5eb916f6bd4e743206913e6f28051249de8ccfd070eae47b5bde31ee813d55f" default)))
+    ("36d92f830c21797ce34896a4cf074ce25dbe0dabe77603876d1b42316530c99d" "b04425cc726711a6c91e8ebc20cf5a3927160681941e06bc7900a5a5bfe1a77f" "c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "b809c70eeba09728804ebfbd16bf40f408c1dbe751b94330ff9e8d14a77b3370" "3625c04fa4b8a802e96922d2db3f48c9cb2f93526e1dc24ba0b400e4ee4ccd8a" "74278d14b7d5cf691c4d846a4bbf6e62d32104986f104c1e61f718f9669ec04b" "fe1682ca8f7a255cf295e76b0361438a21bb657d8846a05d9904872aa2fb86f2" "26ce7eea701bfd143ac536e6805224cff5598b75effb60f047878fe9c4833ae4" "8577da1641ed4bdf255341ca92e3d0e49c9f4d574458f09ce78159690442cade" "e8825f26af32403c5ad8bc983f8610a4a4786eb55e3a363fa9acb48e0677fe7e" "cdd26fa6a8c6706c9009db659d2dffd7f4b0350f9cc94e5df657fa295fffec71" "1012cf33e0152751078e9529a915da52ec742dabf22143530e86451ae8378c1a" "9d7e517b49068e9fef941fe4083ad3d2a4b040895dca5175b84be48739689707" "38ba6a938d67a452aeb1dada9d7cdeca4d9f18114e9fc8ed2b972573138d4664" "40bc0ac47a9bd5b8db7304f8ef628d71e2798135935eb450483db0dbbfff8b11" "603a9c7f3ca3253cb68584cb26c408afcf4e674d7db86badcfe649dd3c538656" "5999e12c8070b9090a2a1bbcd02ec28906e150bb2cdce5ace4f965c76cf30476" "badc4f9ae3ee82a5ca711f3fd48c3f49ebe20e6303bba1912d4e2d19dd60ec98" "519d1b3cb7345cc9be10b4b0489436ae2d1b0690470d8d78f8e4e1ff19b83a86" "5dd70fe6b64f3278d5b9ad3ff8f709b5e15cd153b0377d840c5281c352e8ccce" "12b7ed9b0e990f6d41827c343467d2a6c464094cbcc6d0844df32837b50655f9" "3d5307e5d6eb221ce17b0c952aa4cf65dbb3fa4a360e12a71e03aab78e0176c5" "f5eb916f6bd4e743206913e6f28051249de8ccfd070eae47b5bde31ee813d55f" default)))
  '(eval-expression-print-length nil)
- '(org-agenda-files (quote ("~/org/emacs.org" "~/org/Krishnamurti.org")))
+ '(fci-rule-color "#3E3D31")
+ '(helm-ag-base-command "ack --nocolor --nogroup")
+ '(highlight-changes-colors (quote ("#FD5FF0" "#AE81FF")))
+ '(highlight-tail-colors
+   (quote
+    (("#3E3D31" . 0)
+     ("#67930F" . 20)
+     ("#349B8D" . 30)
+     ("#21889B" . 50)
+     ("#968B26" . 60)
+     ("#A45E0A" . 70)
+     ("#A41F99" . 85)
+     ("#3E3D31" . 100))))
+ '(ledger-reports
+   (quote
+    (("cleared" "ledger -f my.ledger cleared")
+     ("bal" "ledger -f %(ledger-file) bal")
+     ("reg" "ledger -f %(ledger-file) reg")
+     ("payee" "ledger -f %(ledger-file) reg @%(payee)")
+     ("account" "ledger -f %(ledger-file) reg %(account)"))))
+ '(magit-diff-use-overlays nil)
+ '(nrepl-message-colors
+   (quote
+    ("#CC9393" "#DFAF8F" "#F0DFAF" "#7F9F7F" "#BFEBBF" "#93E0E3" "#94BFF3" "#DC8CC3")))
+ '(org-agenda-files (quote ("~/org/officework.org" "~/org/emacs.org")))
+ '(pos-tip-background-color "#A6E22E")
+ '(pos-tip-foreground-color "#272822")
  '(pyim-dicts
    (quote
-    ((:name "BigDict-01" :file "/Users/cmal/.emacs.d/pyim/dicts/pyim-bigdict.pyim" :coding utf-8-unix :dict-type pinyin-dict)))))
+    ((:name "BigDict-01" :file "/Users/cmal/.emacs.d/pyim/dicts/pyim-bigdict.pyim" :coding utf-8-unix :dict-type pinyin-dict))))
+ '(sml/replacer-regexp-list
+   (quote
+    (("^~/org" ":Org:")
+     ("^~/\\.emacs\\.d/elpa/" ":ELPA:")
+     ("^~/\\.emacs\\.d/" ":ED:")
+     ("^/sudo:.*:" ":SU:")
+     ("^~/Documents/" ":Doc:")
+     ("^~/Dropbox/" ":DB:")
+     ("^:\\([^:]*\\):Documento?s/" ":\\1/Doc:")
+     ("^~/[Gg]its/" ":Git:")
+     ("^~/[Gg]it[Hh]ub/" ":Git:")
+     ("^~/[Gg]it\\([Hh]ub\\|\\)-?[Pp]rojects/" ":Git:"))))
+ '(vc-annotate-background nil)
+ '(vc-annotate-color-map
+   (quote
+    ((20 . "#F92672")
+     (40 . "#CF4F1F")
+     (60 . "#C26C0F")
+     (80 . "#E6DB74")
+     (100 . "#AB8C00")
+     (120 . "#A18F00")
+     (140 . "#989200")
+     (160 . "#8E9500")
+     (180 . "#A6E22E")
+     (200 . "#729A1E")
+     (220 . "#609C3C")
+     (240 . "#4E9D5B")
+     (260 . "#3C9F79")
+     (280 . "#A1EFE4")
+     (300 . "#299BA6")
+     (320 . "#2896B5")
+     (340 . "#2790C3")
+     (360 . "#66D9EF"))))
+ '(vc-annotate-very-old-color nil)
+ '(weechat-color-list
+   (unspecified "#272822" "#3E3D31" "#A20C41" "#F92672" "#67930F" "#A6E22E" "#968B26" "#E6DB74" "#21889B" "#66D9EF" "#A41F99" "#FD5FF0" "#349B8D" "#A1EFE4" "#F8F8F2" "#F8F8F0"))
+ '(when
+      (or
+       (not
+	(boundp
+	 (quote ansi-term-color-vector)))
+       (not
+	(facep
+	 (aref ansi-term-color-vector 0))))))
 
 ;; to custom the identification of stuck project
 (setq-default org-stuck-projects
@@ -363,7 +438,7 @@
 
 
 (require 'ace-jump-mode)
-(define-key global-map (kbd "C-c SPC") 'ace-jump-mode)
+;(define-key global-map (kbd "C-c SPC") 'ace-jump-mode)
 
 
 (load-file "~/.emacs.d/site-lisp/web-mode.el")
@@ -389,7 +464,7 @@
 ;(add-to-list 'load-path
 ;             (expand-file-name "/path/to/ledger/source/lisp/"))
 (add-to-list 'auto-mode-alist '("\\.ledger$" . ledger-mode))
-(load-file "~/gits/ledger/my-ledger.el")
+;;(load-file "~/gits/ledger/my-ledger.el")
 
 (global-anzu-mode 1)
 
@@ -414,3 +489,99 @@
 (key-chord-define-global "gd" 'mc/mark-all-symbols-like-this)
 (key-chord-define-global "gg" 'mc/mark-next-like-this-symbol)
 (key-chord-define-global "gp" 'mark/previous-symbol-like-this)
+
+
+
+;; helm-xcdoc
+;; to search document
+;; M-x helm-xcdoc-search
+;; M-x helm-xcdoc-search-other-window
+(require 'helm-xcdoc)
+(setq helm-xcdoc-command-path "/Applications/Xcode.app/Contents/Developer/usr/bin/docsetutil")
+(setq helm-xcdoc-document-path "~/Library/Developer/Shared/Documentation/DocSets/com.apple.adc.documentation.AppleiOS8.1.iOSLibrary.docset")
+
+;; MarkDown mode
+(autoload 'markdown-mode "markdown-mode"
+  "Major mode for editing Markdown files" t)
+(add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+
+;; cursor-chg
+;;(require 'cursor-chg)
+;;(change-cursor-mode 1)
+;;(toggle-cursor-type-when-idle 1)
+
+;; corsshairs, to highlight current line and column
+;; (crosshairs-mode 1)
+
+;; on-screen, highlight when scroll
+;(require 'on-screen)
+;(on-screen-global-mode +1)
+
+
+;; helm-ag, search with helm
+
+
+
+;; fcitx-remote-os-x --with-input-method=sogou-pinyin
+(load-file "~/.emacs.d/site-lisp/fcitx.el")
+(require 'fcitx)
+
+(require 'highlight-current-line)
+(highlight-current-line-on t)
+
+;;golden-ratio-scroll-screen
+(require 'golden-ratio-scroll-screen)
+(global-set-key [remap scroll-down-command] 'golden-ratio-scroll-screen-down)
+(global-set-key [remap scroll-up-command] 'golden-ratio-scroll-screen-up)
+
+;;(require 'diredful)
+;;(diredful-mode 1)
+
+(require 'dash)
+(require 'blog-admin)
+;; (setq blog-admin-backend-path "~/gits/blog")
+;; (setq blog-admin-backend-type 'hexo)
+;; (setq blog-admin-backend-new-post-in-drafts t) ;; create new post in drafts by default
+;; (setq blog-admin-backend-new-post-with-same-name-dir t) ;; create same-name directory with new post
+(setq blog-admin-backend-type 'org-page)
+(setq blog-admin-backend-path "~/gits/op-blog")
+(setq blog-admin-backend-new-post-in-drafts t)
+(setq blog-admin-backend-new-post-with-same-name-dir t)
+(setq blog-admin-backend-org-page-drafts "_drafts")
+
+
+(eval-after-load 'org '(require 'org-pdfview))
+
+(require 'org-page)
+(setq op/repository-directory "~/gits/op-blog")
+(setq op/site-domain "http://localhost:8080")
+;;; for commenting, you can choose either disqus or duoshuo
+(setq op/personal-disqus-shortname "zyzy5730")
+;(setq op/personal-duoshuo-shortname "cmal")
+;;; the configuration below are optional
+;;(setq op/personal-google-analytics-id "your_google_analytics_id")
+
+(setq op/site-main-title "M-x psychotherapist")
+(setq op/site-sub-title "cmal@bitbucket.org")
+(setq op/personal-github-link "https://github.com/cmal")
+;; (setq op/category-config-alist
+;;       (cons '("None" category name goes here to disable commenting
+;; 	      :show-comment nil)
+;; 	    op/category-config-alist))
+
+;;(require 'org-alert)
+;;(org-alert-enable)
+(rainbow-mode 1)
+(sml/setup)
+;(sml/apply-theme "light-powerline")
+;; (diredful-add CFG-FILE-NAME)
+
+(require 'ace-pinyin)
+(ace-pinyin-global-mode 1)
+
+;; helm-swoop
+;;(require 'helm-swoop)
+
+;; pinyin-search
+(define-key global-map (kbd "C-c SPC") 'ace-pinyin-jump-char)
