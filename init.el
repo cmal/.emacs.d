c68bcfbdfdb149b9f210c905a61e6d1da5f7df93
赵宇 Yu Zhao <cmal@zhaoyutekiMacBook-Pro.local>
2016-12-24 15:12:40 +0800

    el-get

--- init.el
+++ init.el
@@ -8,9 +8,6 @@
 ;; You may delete these explanatory comments.
 (package-initialize)
 
-;; add el-get libs before loading custom.el
-(el-get-bundle dholm/tabbar)
-(require 'tabbar)
 
 (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
@@ -185,7 +182,7 @@
      ;; e2wm
      swbuff
      swbuff-x
-
+     el-get
      atomic-chrome
      )))
 
@@ -378,17 +375,20 @@
 ;; ;; el-get
 ;; (add-to-list 'load-path "~/.emacs.d/site-lisp/el-get")
 
-;; (unless (require 'el-get nil 'noerror)
-;;   (require 'package)
-;;   (add-to-list 'package-archives
-;;                '("melpa" . "http://melpa.org/packages/"))
-;;   (package-refresh-contents)
-;;   (package-initialize)
-;;   (package-install 'el-get)
-;;   (require 'el-get))
+(unless (require 'el-get nil 'noerror)
+  (require 'package)
+  (add-to-list 'package-archives
+               '("melpa" . "http://melpa.org/packages/"))
+  (package-refresh-contents)
+  (package-initialize)
+  (package-install 'el-get)
+  (require 'el-get))
 ;; (add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user/recipes")
 ;; (el-get 'sync)
 
+;; add el-get libs before loading custom.el
+(el-get-bundle dholm/tabbar)
+(require 'tabbar)
 
 ;; el-get install github packages
 ;; (el-get-bundle tarao/tab-group-el)
