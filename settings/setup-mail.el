9622563772683e0d3781a994448cdaa850a7d6d6
yuzhao <yu.zhao@joudou.com>
2017-06-28 14:31:30 +0800

    move midnight to setup-mail.el

--- settings/setup-mail.el
+++ settings/setup-mail.el
@@ -21,4 +21,28 @@
 
 (add-hook 'message-mode-hook 'message-mode-custom-keys-config)
 
+
+;; ==========
+;; send mail at specific time
+(require 'midnight)
+(midnight-delay-set 'midnight-delay "19:00pm")
+;; or (midnight-delay-set 'midnight-delay 16200) ;; (eq (* 4.5 60 60) "4:30am")
+(add-hook 'midnight-hook (lambda ()
+                           (with-current-buffer "*unsent mail*"
+                             (call-interactively 'message-send))))
+(add-hook 'midnight-hook 'calendar)
+;; (cancel-timer midnight-timer)
+;; (setq midnight-period 7200) ;; (eq (* 2 60 60) "2 hours")
+;; ==========
+
+
+(defun send-month-check-mail-at-some-time ()
+  (interactive)
+  (run-at-time
+   (encode-time 59 21 14 30 6 2017) ;; TODO should change this to month end
+   nil
+   (lambda ()
+     (with-current-buffer "*monthcheck mail*"
+       (call-interactively 'message-send)))))
+
 (provide 'setup-mail)
