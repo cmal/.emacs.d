e79d59aa202d78b175de02d063b4029e2ab1f704
Yu Zhao <yu.zhao@joudou.com>
2018-02-22 17:55:37 +0800

    update

--- settings/setup-java.el
+++ settings/setup-java.el
@@ -3,5 +3,24 @@
       (append '(("\\.java\\'" . jdee-mode)) auto-mode-alist))
 (custom-set-variables
  '(jdee-server-dir "/Users/yuzhao/.emacs.d/jdee-server"))
+
+;; only for thinking-in-java
+(setq mindview-jar
+      "/Library/Java/JavaVirtualMachines/jdk1.8.0_102.jdk/Contents/Home/lib/net.jar")
+
+(custom-set-variables
+ '(jdee-compile-option-command-line-args
+   `("-classpath"
+     ,mindview-jar)))
+
+(custom-set-variables
+ '(jdee-run-option-classpath
+   `(,mindview-jar ".")))
+
+(add-hook
+ 'jdee-mode-hook
+ '(lambda ()
+    (local-set-key (kbd "RET") 'newline-and-indent)))
+
 (require 'jdee)
 (provide 'setup-java)
