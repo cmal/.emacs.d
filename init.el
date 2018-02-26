be93ea1af9ee2a9a501d6d6f03c52d5b993f8cda
Yu Zhao <yu.zhao@joudou.com>
2018-02-26 13:50:23 +0800

    update

--- init.el
+++ init.el
@@ -119,41 +119,48 @@
 ;; NOTE: folder `jdee-sever/` has been added for jdee to work.
 (defun init--install-packages ()
   (packages-install
-   (remove nil
-   '(magit git-timemachine edn uuid dash diminish inflections spinner
-           move-text golden-ratio-scroll-screen yasnippet emmet-mode
-           json-mode helm helm-org-rifle helm-descbinds org-redmine
-           org-repo-todo helm-ack helm-ls-git helm-emmet visual-regexp
-           fill-column-indicator flycheck flycheck-pos-tip
-           flycheck-clojure undo-tree smartscan smartparens guide-key
-           sx highlight-escape-sequences whitespace-cleanup-mode
-           elisp-slime-nav
-           eval-sexp-fu clojure-mode
-           align-cljlet
-           clj-refactor cljr-helm clojure-mode-extra-font-locking
-           cider cider-eval-sexp-fu company ac-cider helm-cider
-           clj-refactor 4clojure helm-clojuredocs helm-cider-history
-           rainbow-mode rainbow-delimiters thingatpt paredit mic-paren
-           yesql-ghosts string-edit multiple-cursors fic-mode
-           smart-forward wgrep change-inner web-mode js2-mode
-           js2-refactor nodejs-repl restclient less-css-mode yaml-mode
-           sicp graphviz-dot-mode blog-admin edit-server keyfreq
-           expand-region web-server adoc-mode bug-hunter helm-chrome
-           helm-github-stars swbuff swbuff-x ace-jump-mode
-           pinyin-search atomic-chrome vue-mode editorconfig sass-mode
-           psysh php-mode jedi jedi-direx wolfram-mode
-           language-detection w3m helm-w3m bookmark+ alert org-alert
-           info+ hyperbole suggest ledger-mode hledger-mode vlf
-           racket-mode exec-path-from-shell pangu-spacing
-           aggressive-indent tree-mode geiser monokai-theme
-           haskell-mode intero helm-ag paredit-menu paredit-everywhere
-           jdee help+ help-fns+ help-mode+ log4j-mode log4e
-           org-wunderlist slack buffer-flip ;; use-package capture
-           multi-term helm-mt ;; chinese-fonts-setup helm-projectile
-           swiper-helm ;; smooth-scrolling auto-complete emms
-           helm-emms ;; emms-player-mpv emms-browser emms-source-file
-           ;; emms-source-playlist emms-lyrics emms-info e2wm el-get
-           ;; beacon )))
+   (append
+    '(magit
+      git-timemachine edn uuid dash diminish inflections spinner
+      move-text golden-ratio-scroll-screen yasnippet emmet-mode
+      json-mode helm helm-org-rifle helm-descbinds org-redmine
+      org-repo-todo helm-ack helm-ls-git helm-emmet
+      visual-regexp fill-column-indicator flycheck
+      flycheck-pos-tip flycheck-clojure undo-tree smartscan
+      smartparens guide-key sx highlight-escape-sequences
+      whitespace-cleanup-mode elisp-slime-nav rainbow-mode
+      rainbow-delimiters thingatpt paredit mic-paren
+      yesql-ghosts string-edit multiple-cursors fic-mode
+      smart-forward wgrep change-inner web-mode js2-mode
+      js2-refactor nodejs-repl restclient less-css-mode
+      yaml-mode sicp graphviz-dot-mode blog-admin edit-server
+      keyfreq expand-region web-server markdown-preview-mode
+      adoc-mode bug-hunter helm-chrome helm-github-stars swbuff
+      swbuff-x ace-jump-mode pinyin-search atomic-chrome
+      vue-mode editorconfig sass-mode psysh php-mode jedi
+      jedi-direx wolfram-mode language-detection w3m helm-w3m
+      bookmark+ alert org-alert info+ hyperbole suggest
+      ledger-mode hledger-mode vlf racket-mode
+      exec-path-from-shell pangu-spacing aggressive-indent
+      tree-mode geiser monokai-theme haskell-mode intero helm-ag
+      paredit-menu paredit-everywhere jdee help+ help-fns+
+      help-mode+ log4j-mode log4e org-wunderlist
+      buffer-flip)
+    (when (not is-android)
+      '(eval-sexp-fu
+        clojure-mode align-cljlet
+        clj-refactor cljr-helm clojure-mode-extra-font-locking
+        cider cider-eval-sexp-fu company ac-cider helm-cider
+        clj-refactor 4clojure helm-clojuredocs helm-cider-history
+        slack)))))
+;; NOT USED:
+;; use-package capture multi-term helm-mt
+;; chinese-fonts-setup helm-projectile swiper-helm
+;; smooth-scrolling auto-complete emms helm-emms
+;; emms-player-mpv emms-browser emms-source-file
+;; emms-source-playlist emms-lyrics emms-info e2wm el-get
+;; beacon
+
 ;; ad--addoit-function
 
 (condition-case nil
