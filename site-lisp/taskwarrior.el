421454dc1801099c5e0c9644f671ac53ffc3ec54
Yu Zhao <yu.zhao10@ele.me>
2019-05-31 22:18:40 +0800

    update: update taskwarrior.el

--- site-lisp/taskwarrior.el
+++ site-lisp/taskwarrior.el
@@ -40,11 +40,28 @@
       (define-key taskwarrior-map (kbd "g") 'taskwarrior)
       (define-key taskwarrior-map (kbd "a") 'taskwarrior-add-task)
       (define-key taskwarrior-map (kbd "d") 'taskwarrior-done-task)
-      (define-key taskwarrior-map (kbd "i") 'taskwarrior-task-info)))
+      (define-key taskwarrior-map (kbd "D") 'taskwarrior-delete-task)
 
-(defun display-tasks ()
-  (let ((shell-output (shell-command-to-string "task"))
-        (taskwarrior-buffer (get-buffer-create taskwarrior-buffer-name)))
+      ;; FIXME: either implementation will get non utf-8 task will 100% CPU
+      ;; (define-key taskwarrior-map (kbd "i") 'taskwarrior-task-info)
+
+      (define-key taskwarrior-map (kbd "p") 'taskwarrior-task-depends-on)
+      (define-key taskwarrior-map (kbd "o") 'taskwarrior-set-priority)
+      (define-key taskwarrior-map (kbd "b") 'taskwarrior-blocked)
+      (define-key taskwarrior-map (kbd "B") 'taskwarrior-blocking)
+      (define-key taskwarrior-map (kbd "C-b") 'taskwarrior-blocking-not-blocked)
+      (define-key taskwarrior-map (kbd "S") 'taskwarrior-start)
+      (define-key taskwarrior-map (kbd "q") 'quit-window)))
+
+(defun task-sync ()
+  (interactive)
+  (shell-command "task sync"))
+
+(defun display-tasks (&optional tag)
+  (task-sync)
+  (let* ((cmd-str (if tag (format "task +%s" tag) "task"))
+         (shell-output (shell-command-to-string cmd-str))
+         (taskwarrior-buffer (get-buffer-create taskwarrior-buffer-name)))
     (switch-to-buffer taskwarrior-buffer)
     (erase-buffer)
     (insert shell-output)))
@@ -52,50 +69,118 @@
 (defun taskwarrior-add-task (task-description)
   "Add taskwarrior task."
   (interactive "sTask description: ")
-  (shell-command (format "task add %s" task-description))
-  (display-tasks))
+  (message task-description)
+  (let ((params (split-string (format "add %s" task-description))))
+    (if (zerop (apply 'call-process (append (list "task" nil nil nil) params)))
+        (display-tasks)
+      (message (concat "Command fail: task " params)))))
 
 (defun filter-spaces (seq)
   "Filter spaces from seq of strings."
   (delq nil (mapcar (lambda (x) (if (string-equal x " ") nil x)) seq)))
 
-(defun get-task-number (line)
+(defun get-task-id (line)
   (let ((tokens (split-string line)))
     (string-to-number (nth 0 (filter-spaces tokens)))))
 
-(defun delete-task ()
+(defun get-current-line-task-id ()
+  (let ((current-line (thing-at-point 'line)))
+    (get-task-id current-line)))
+
+(defun taskwarrior-delete-task ()
   (interactive)
-  (let* ((current-line (thing-at-point 'line))
-         (task-number (get-task-number current-line)))
-      (message (format "task %i delete" (string-to-number task-number)))))
+  (let* ((task-id (get-current-line-task-id)))
+      (message (format "task %i delete" (string-to-number task-id)))))
 
 (defun taskwarrior-done-task ()
   "Mark taskwarrior task as done."
   (interactive)
   (let* ((current-line (thing-at-point 'line))
-         (task-number (get-task-number current-line)))
-    (if (/= 0 task-number)
-        (shell-command (format "task %i done" task-number)))
+         (task-id (get-task-id current-line)))
+    (if (/= 0 task-id)
+        (shell-command (format "task %i done" task-id)))
     (display-tasks)))
 
-(defun taskwarrior-task-info ()
-  "Show task detailed information."
+(defun taskwarrior-modify (str)
+  "Modify current task"
+  (let ((task-id (get-current-line-task-id)))
+    (shell-command (format "task %d modify %s" task-id str))
+    (display-tasks)))
+
+(defun taskwarrior-task-depends-on (dep-id)
+  "Modify current task to depends on another task by id"
+  (interactive "nCurrent task depends on: ")
+  (taskwarrior-modify (format "depends:%d" dep-id)))
+
+(defun taskwarrior-set-priority (c)
+  "Modify current task to priority(H/L/M)."
+  (interactive "cSet task Priority (H/L/M): ")
+  (when (member c '(?M ?m ?L ?l ?H ?h))
+    (taskwarrior-modify (format "priority:%c" c))
+    (message "Only H/L/M are accepted.")))
+
+(defun taskwarrior-blocked ()
+  "Display only those tasks which depend on another task."
   (interactive)
-  (let* ((current-line (thing-at-point 'line))
-         (task-number (get-task-number current-line)))
-    (if (/= 0 task-number)
-        (with-output-to-temp-buffer (shell-command (format "task %i info" task-number))))))
+  (display-tasks "BLOCKED"))
+
+(defun taskwarrior-blocking ()
+  "Display only those tasks which are assigned as dependencies for another task."
+  (interactive)
+  (display-tasks "BLOCKING"))
+
+(defun taskwarrior-blocking-not-blocked ()
+  "Display what is blocking and is not blocked,
+focusing on the tasks that block everything else."
+  (interactive)
+  (display-tasks "BLOCKING -BLOCKED"))
+
+(defun taskwarrior-start ()
+  "Modify current task to depends on another task by id"
+  (interactive)
+  (let ((task-id (get-current-line-task-id)))
+    (shell-command (format "task %d start" task-id))
+    (display-tasks)))
+
+;; (defun taskwarrior-task-info ()
+;;   "Show task detailed information."
+;;   (interactive)
+;;   (let ((task-id (get-current-line-task-id)))
+;;     (when (not (zerop task-id))
+;;       (set-buffer (get-buffer-create "*task-info*"))
+;;       (buffer-disable-undo)
+;;       (erase-buffer)
+;;       (let ((process (start-process-shell-command
+;;                       (format "task-info-%d" (random))
+;;                       "*task-info*"
+;;                       (format "task %d info" task-id))))
+;;         (set-process-sentinel
+;;          process
+;;          (lambda (process signal)
+;;            (when (memq (process-status process) '(exit signal))
+;;              (unless (string= (buffer-name) "*task-info*")
+;;                (switch-to-buffer-other-window "*task-info*")
+;;                (require 'ansi-color)
+;;                (ansi-color-apply-on-region (point-min) (point-max))
+;;                (local-set-key (kbd "q") 'quit-window)
+;;                (goto-char (point-min))))))))))
+
+;; (defun my-shell-execute (cmd bf-name)
+;;   (shell (get-buffer-create bf-name))
+;;    (process-send-string (get-buffer-process bf-name) (concat cmd "\n")))
+
+;; (defun taskwarrior-task-info ()
+;;   "Show task detailed information."
+;;   (interactive)
+;;   (let ((task-id (get-current-line-task-id)))
+;;     (when (/= 0 task-id)
+;;       (my-shell-execute (format "task %d info" task-id) "*task-info*"))))
 
 (defun taskwarrior ()
   "The entry point for taskwarrior client."
   (interactive)
   (display-tasks)
-  (use-local-map taskwarrior-map)
-  )
-
-(defun task-sync ()
-  (interactive)
-  (shell-command "task sync"))
+  (use-local-map taskwarrior-map))
 
 (provide 'taskwarrior)
 ;;; taskwarrior.el ends here
