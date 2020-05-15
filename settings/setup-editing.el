efc4c5bec2fcd89712391ab3839d3aadfd4be17e
Yu Zhao <{ID}+{username}@users.noreply.github.com>
2020-05-15 17:36:35 +0800

    fix: update

--- settings/setup-editing.el
+++ settings/setup-editing.el
@@ -390,64 +390,6 @@ instead."
 ;; \([0-9]+\)px
 ;; \,(double-px \1)px
 
-(defun vc-git-grep2 (regexp dir)
-  (interactive  ;; interactive 后的内容的返回值被作为整个函数的参数传入
-   (progn
-     (grep-compute-defaults)
-     (cond
-      ((equal current-prefix-arg '(16))
-       (list (read-from-minibuffer "Run: " "git grep" nil nil 'grep-history)
-    nil))
-      (t (let* ((regexp (grep-read-regexp))
-    (dir (read-directory-name "In directory: " nil default-directory t)))
-     (list regexp dir))))))
-  (require 'grep)
-  (when (and (stringp regexp) (> (length regexp) 0))
-    (let ((command regexp))
-      (if (> 4 5)
-    (if (string= command "git grep")
-     (setq command nil))
-  (setq dir (file-name-as-directory (expand-file-name dir)))
-  (setq command
-     (grep-expand-template "git grep -n -i -e <R>" regexp))
-  (when command
-    (if (equal current-prefix-arg '(4))
-     (setq command
-     (read-from-minibuffer "Confirm: " command nil nil 'grep-history))
-   (add-to-history 'grep-history command))))
-      (when command
-  (let ((default-directory dir)
-     (compilation-environment '("PAGER=")))
-    ;; Setting process-setup-function makes exit-message-function work
-    ;; even when async processes aren't supported.
-    (compilation-start command 'grep-mode))
-  (if (eq next-error-last-buffer (current-buffer))
-      (setq default-directory dir))))))
-
-(defun vc-git-grep3 (regexp)
-  (interactive
-   (progn
-     (grep-compute-defaults)
-     (cond
-      ((equal current-prefix-arg '(16))
-       (list (read-from-minibuffer "Run: " "git grep" nil nil 'grep-history)))
-      (t (list (grep-read-regexp))))))
-  (require 'grep)
-  (when (and (stringp regexp) (> (length regexp) 0))
-    (let ((command regexp))
-      (setq command
-            (grep-expand-template "git grep -n -i -e <R>" regexp))
-      (when command
-        (if (equal current-prefix-arg '(4))
-            (setq command
-                  (read-from-minibuffer "Confirm: " command nil nil 'grep-history))
-          (add-to-history 'grep-history command)))
-      (when command
-        (let ((compilation-environment '("PAGER=")))
-          ;; Setting process-setup-function makes exit-message-function work
-          ;; even when async processes aren't supported.
-          (compilation-start command 'grep-mode))))))
-
 (use-package helm-git-grep
   :ensure t
   :demand t)
