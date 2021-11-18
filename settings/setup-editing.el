5c94bb14ae919aea6aae1aa474e1ce92430b9597
Yu Zhao <yu.zhao@cicc.com.cn>
2021-11-18 20:01:22 +0800

    fix: remove chinese-wubi

--- settings/setup-editing.el
+++ settings/setup-editing.el
@@ -168,10 +168,11 @@
       (lambda (&optional arg) "Keyboard macro." (interactive "p") (kmacro-exec-ring-item (quote ([4 134217790 backspace 134217788 134217848 114 101 112 108 97 99 101 45 115 116 114 105 110 103 return 44 32 return 17 10 return 134217788 134217848 114 101 112 108 97 99 101 45 115 116 114 105 110 103 return 58 return 39 return 134217788 134217848 114 101 112 108 97 99 101 45 115 116 114 105 110 103 return 32 return 44 return] 0 "%d")) arg)))
 
 ;; input method
-(use-package chinese-wubi
-  :config
-  (register-input-method "chinese-wubi" "Chinese-GB" 'quail-use-package "WB" "汉字输入∷五笔输入法∷")
-  (setq default-input-method "chinese-wubi"))
+(comment
+ (use-package chinese-wubi
+   :config
+   (register-input-method "chinese-wubi" "Chinese-GB" 'quail-use-package "WB" "汉字输入∷五笔输入法∷")
+   (setq default-input-method "chinese-wubi")))
 ;; (toggle-input-method)
 
 ;; (when mac-p
